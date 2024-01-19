8490247a1fafd5f57aa6e21b93a987d68912126e
John Moser <john.r.moser@gmail.com>
2024-01-18 23:04:48 -0500

    Updated timing based on all 3 datasheets

--- code/firmware/ay38930/interface.py
+++ code/firmware/ay38930/interface.py
@@ -36,9 +36,25 @@ class PIOInterface:
     #   |0000 0000 000|data     |fnc   |pindirs |
 
     # The latched I/O interface is for the 5V AY-3-89xx and YM2149 and
-    # is divorced from the clock. It has 300ns setup and 65ns hold.
-    # Each clock tick must be more than 75ns (13MHz) to meet the below
-    # timing.
+    # is divorced from the clock.  Timings in ns are:
+    # 
+    #                8910  YM2149  8930
+    # Address setup   400     300   300
+    # Address hold    100      80    65
+    # Write setup      50       0   300      
+    # Write pulse    1800     300     *
+    # Write hold      100      80    65
+    # Read access     350     400   200
+    #
+    # The 8930 has no real "write setup"; rather, it must have the data
+    # on the bus for 300ns before lowering the control bus, and 65ns
+    # after.  The others need the data on the bus before setting data
+    # write on the control bus.  Address "setup" on all chips works the
+    # the same as the 8930 data "setup."
+    #
+    # Each clock tick must be more than 100ns (10MHz) to meet the above
+    # timing.  Writes are stretched out a lot because of the long write
+    # pulse of the 8910.
     #
     # OSR       *
     # bc1/bdir  ___----_
@@ -49,10 +65,9 @@ class PIOInterface:
     @rp2.asm_pio()
     def latched_io(self):
         label("reenter")
-        # Close all latches
-        nop() .side(0b00)
+        # Close all latches while spinning
         .wrap_target()
-        jmp(not_osre, "handle")
+        jmp(not_osre, "handle") .side(0b00)
         wrap()
 
         # Handle i/o, jump to location given on input.
@@ -64,17 +79,22 @@ class PIOInterface:
         out(pins, 8) .side(0b11) [3]
         jmp("reenter")
 
+        # Long write pulse for the original 8910
         label("set data")
-        out(pins, 8) .side(0b10) [3]
+        out(pins, 8)
+        set(x, 3) .side(0b10) [3]
+        label("write pulse")
+        jmp(dec_x, "write pulse") [3]
         jmp("reenter")
 
         # Data is read on the falling edge in the original AY-3
         label("get data")
         # Reading, so discard the last 8 bits of OSR
-        # Data READ setup time is max 200ns, hold is 100ns, so 225ns
-        # and then in()
-        out(null, 8) .side(0b01) [2]
+        # Data READ access time is max 350ns, so delay to 400ns
+        out(null, 8) .side(0b01) [3]
         in(pins, 8)
+        # Reset the control bus ASAP because it may take 400ns to
+        # settle after reading
         jmp("reenter") .side(0b00)
 
     # The sync_io() interface is designed for 96kHz sample rate chips
