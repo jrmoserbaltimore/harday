6458af34e6c33793ed938e0262444b3f92f2426c
John Moser <john.r.moser@gmail.com>
2024-01-18 21:55:24 -0500

    Rework latched I/O PIO program

--- code/firmware/ay38930/interface.py
+++ code/firmware/ay38930/interface.py
@@ -10,14 +10,13 @@ class RegisterInterface:
         pass
 
 class PIOInterface:
-    __init__(self, da_base: Pin, bc1: Pin, bdir: Pin, clock: Pin, reset: Pin):
+    __init__(self, da_base: Pin, bc1: Pin, clock: Pin, reset: Pin):
         assert freq() % (2*10**6) == 0, "machine.freq must be divisible by 2MHz"
 
         self.pio_req = freq() / 2000000
 
         self.da_pins = da_pins
         self.bc1_pin = bc1
-        self.bdir_pin = bdir
         self.clock_pin = clock
         self.reset_pin = reset
         self._program_pio()
@@ -32,34 +31,56 @@ class PIOInterface:
         irq(1)
         set(pins,0)
 
-    # Sets IRQ 2 and waits for it to be 0, then sets pins to out.
-    # Sets IRQ 3 and waits for it to be 0, then sets pins to in.
-    # Clearing 2 when the pins are already out, or 3 when pins are in,
-    # doesn't affect execution flow.
-    @rp2.asm_pio(set_init=rp2.PIO.OUT_LOW)
-    def da_direction(self):
-        wrap_target()
-        mov(osr,invert(null))
-        irq(block, 2)
-        out(pindirs,8)
-        mov(osr,null)
-        irq(block, 3)
-        out(pindirs,8)
+    # OSR format:
+    #   |31-21        |20-13    |12-8  |7-0     |
+    #   |0000 0000 000|data     |fnc   |pindirs |
+
+    # The latched I/O interface is for the 5V AY-3-89xx and YM2149 and
+    # is divorced from the clock. It has 300ns setup and 65ns hold.
+    # Each clock tick must be more than 75ns (13MHz) to meet the below
+    # timing.
+    #
+    # OSR       *
+    # bc1/bdir  ___----_
+    # da           *
+    # pindir     *
+    # delay         +++
+
+    @rp2.asm_pio()
+    def latched_io(self):
+        label("reenter")
+        # Close all latches
+        nop() .side(0b00)
+        .wrap_target()
+        jmp(not_osre, "handle")
         wrap()
 
-    # Following IRQ 0:
-    #  - 5 instructions for set address to reach falling edge
-    #  - Same for writing data
-    #  - 3 and then wait for falling edge to read data
-    # Following IRQ 1:
-    #  - 0 instruction and then wait for rising edge for DA
-    #  - 2 instruction and then wait for rising edge for read
-    @rp2.asm_pio()
-    def latched_io:
-        label("finish write")
-        # wait for falling edge, then clear the control pins
-        wait(1, irq, 1) .side(0b00)
+        # Handle i/o, jump to location given on input.
+        label("handle")
+        out(pindirs, 8)
+        out(pc, 5)
 
+        label("set address")
+        out(pins, 8) .side(0b11) [3]
+        jmp("reenter")
+
+        label("set data")
+        out(pins, 8) .side(0b10) [3]
+        jmp("reenter")
+
+        # Data is read on the falling edge in the original AY-3
+        label("get data")
+        # Reading, so discard the last 8 bits of OSR
+        # Data READ setup time is max 200ns, hold is 100ns, so 225ns
+        # and then in()
+        out(null, 8) .side(0b01) [2]
+        in(pins, 8)
+        jmp("reenter") .side(0b00)
+
+    # The sync_io() interface is designed for 96kHz sample rate chips
+    # running at 9.216MHz or 24.576MHz.
+    @rp2.asm_pio()
+    def sync_io(self):
         label("reenter")
         # Loop until a rising edge occurs with something in the OSR
         .wrap_target()
@@ -69,25 +90,9 @@ class PIOInterface:
 
         # Handle i/o, jump to location given on input.
         label("handle")
+        out(pindirs, 8)
         out(pc, 5)
-
-        label("set address")
-        # Ensure DA are out pins
-        irq(clear, 2)
-        out(pins, 8) .side(0b11)
-        jmp("finish write")
-
-        label("set data")
-        irq(clear, 2)
-        out(pins, 8) .side(0b10)
-        jmp("finish write")
-
-        label("get data")
-        # Ensure DA are in pins
-        irq(clear, 3) .side(0b01)
-        wait(1, irq, 1)
-        in(pins, 8)
-        jmp("reenter") .side(0b00)
+        # FIXME:  Implement this
 
     def _write_address(self, address:int):
         # FIXME:  Send the address and the 5-bit jump target
